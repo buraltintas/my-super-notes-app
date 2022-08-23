4ef75e9942e8fb7d3ad0a9641e872846095fd871
buraltintas <burak.altintas@yahoo.com.tr>
2022-08-23 12:05:22 +0300

    add tooltip to action icons in note container

--- src/components/note/index.js
+++ src/components/note/index.js
@@ -4,6 +4,8 @@ import { appContext } from '../../context';
 import { doc, deleteDoc, updateDoc } from 'firebase/firestore';
 import EditNote from '../edit-note';
 import DeleteNote from '../delete-note';
+import Tooltip from '@mui/material/Tooltip';
+import Zoom from '@mui/material/Zoom';
 
 const Note = (props) => {
   const [showEdit, setShowEdit] = useState(false);
@@ -82,24 +84,114 @@ const Note = (props) => {
             <div className={styles.noteButtonsContainer}>
               {props.note.isDone === true ? (
                 <div>
-                  <svg
-                    onClick={() => noteDoneHandler(props.note.id, false)}
-                    className={styles.icon}
-                    xmlns='http://www.w3.org/2000/svg'
-                    viewBox='0 0 20 20'
-                    fill='currentColor'
+                  <Tooltip
+                    title={
+                      <h1
+                        style={{
+                          color: 'rgb(243, 243, 245)',
+                          fontFamily: 'Fira Sans',
+                        }}
+                      >
+                        Make It Undone
+                      </h1>
+                    }
+                    placement='top'
+                    arrow
+                    TransitionComponent={Zoom}
                   >
-                    <path
-                      fillRule='evenodd'
-                      d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
+                    <svg
+                      onClick={() => noteDoneHandler(props.note.id, false)}
+                      className={styles.icon}
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 20 20'
+                      fill='currentColor'
+                    >
+                      <path
+                        fillRule='evenodd'
+                        d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
+                        clipRule='evenodd'
+                      />
+                    </svg>
+                  </Tooltip>
                 </div>
               ) : (
                 <div>
+                  <Tooltip
+                    title={
+                      <h1
+                        style={{
+                          color: 'rgb(243, 243, 245)',
+                          fontFamily: 'Fira Sans',
+                        }}
+                      >
+                        Make It Done
+                      </h1>
+                    }
+                    placement='top'
+                    arrow
+                    TransitionComponent={Zoom}
+                  >
+                    <svg
+                      onClick={() => noteDoneHandler(props.note.id, true)}
+                      className={styles.icon}
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 20 20'
+                      fill='currentColor'
+                    >
+                      <path
+                        fillRule='evenodd'
+                        d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
+                        clipRule='evenodd'
+                      />
+                    </svg>
+                  </Tooltip>
+                </div>
+              )}
+              <div>
+                <Tooltip
+                  title={
+                    <h1
+                      style={{
+                        color: 'rgb(243, 243, 245)',
+                        fontFamily: 'Fira Sans',
+                      }}
+                    >
+                      Edit
+                    </h1>
+                  }
+                  placement='top'
+                  arrow
+                  TransitionComponent={Zoom}
+                >
                   <svg
-                    onClick={() => noteDoneHandler(props.note.id, true)}
+                    onClick={showEditForm}
+                    className={styles.icon}
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 20 20'
+                    fill='currentColor'
+                  >
+                    <path d='M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z' />
+                  </svg>
+                </Tooltip>
+              </div>
+              <div>
+                <Tooltip
+                  title={
+                    <h1
+                      style={{
+                        color: 'rgb(243, 243, 245)',
+                        fontFamily: 'Fira Sans',
+                      }}
+                    >
+                      Delete
+                    </h1>
+                  }
+                  placement='top'
+                  arrow
+                  TransitionComponent={Zoom}
+                >
+                  <svg
+                    onClick={openDeleteForm}
                     className={styles.icon}
                     xmlns='http://www.w3.org/2000/svg'
                     viewBox='0 0 20 20'
@@ -107,37 +199,11 @@ const Note = (props) => {
                   >
                     <path
                       fillRule='evenodd'
-                      d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
+                      d='M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z'
                       clipRule='evenodd'
                     />
                   </svg>
-                </div>
-              )}
-              <div>
-                <svg
-                  onClick={showEditForm}
-                  className={styles.icon}
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 20 20'
-                  fill='currentColor'
-                >
-                  <path d='M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z' />
-                </svg>
-              </div>
-              <div>
-                <svg
-                  onClick={openDeleteForm}
-                  className={styles.icon}
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 20 20'
-                  fill='currentColor'
-                >
-                  <path
-                    fillRule='evenodd'
-                    d='M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z'
-                    clipRule='evenodd'
-                  />
-                </svg>
+                </Tooltip>
               </div>
             </div>
             {renderDate(props.note.time)}
